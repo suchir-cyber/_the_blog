d69a2b683e9b9e2788727c840a92ef721d93f8f4
suchir_18 <97435643+suchir-cyber@users.noreply.github.com>
2024-12-18 00:49:56 +0530

    Updated settings.py

--- blog_app/blog_app/settings.py
+++ blog_app/blog_app/settings.py
@@ -143,6 +143,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'suchirpandula@gmail.com'
-EMAIL_HOST_PASSWORD = 'qrwr lgex rdff glkb'
+EMAIL_HOST_USER = '<your_mail@gmail.com>'
+EMAIL_HOST_PASSWORD = '<your app password>'
 
